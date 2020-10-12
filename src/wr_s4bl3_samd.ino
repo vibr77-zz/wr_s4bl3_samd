ef1e71ec61d3a5ff27531468ba9699d6ac5f5edb
Vincent Besson <vincent@besson.be>
2020-10-12 19:37:28 +0200

    OK

--- src/wr_s4bl3_samd.ino
+++ src/wr_s4bl3_samd.ino
@@ -1,7 +1,7 @@
 //  --------------------------------------------------------------------------
 //  WaterRower S4 BLE Interface
 //  Hardware: Using GATT Fitness Machine Service & Rower Data Characteristics 
-//  Version: 0.14
+//  Version: 0.15
 //  Date: 2020/10/10
 //  Author: Vincent Besson
 //  Note: Testing on Adafruit Feather MO BLE Express
@@ -184,10 +184,9 @@ struct rowerDataKpi rdKpi;
 
 void initBLE(){
   randomSeed(micros());
-  //boolean success;
 
   /* Initialise the module */
-  SerialDebug.print(F("Initialising the Bluefruit LE module: "));
+  SerialDebug.print(F("Initialising the Bluefruit LE module:"));
 
   if ( !ble.begin(VERBOSE_MODE) ){
     error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
@@ -232,12 +231,12 @@ void initBLE(){
     error(F("Could not add Fitness Machine Rower Data characteristic"));
   }
 
-  SerialDebug.println(F("Adding Fitness Machine Service UUID to the advertising payload:: "));
+  SerialDebug.println(F("Adding Fitness Machine Service UUID to the advertising payload "));
   uint8_t advdata[] { 0x02, 0x01, 0x06, 0x05, 0x02, 0x26, 0x18, 0x0a, 0x18 };
   ble.setAdvData( advdata, sizeof(advdata) );
   
   /* Reset the device for the new service setting changes to take effect */
-  SerialDebug.println(F("Performing a SW reset (service changes require a reset): "));
+  SerialDebug.println(F("Performing a SW reset (service changes require a reset)"));
   ble.reset();
 
   SerialDebug.println();
@@ -266,7 +265,6 @@ void initFakeBleData(){
   rdKpi.elapsedTimeHour=1;
   rdKpi.remainingTime=600;
 
-  
 }
 
 void sendFakeBleDataP1(){
@@ -364,6 +362,87 @@ void initBleData(){
   rdKpi.elapsedTimeHour=0;
   rdKpi.remainingTime=0;
 }
+
+void sendBleLightData(){
+  // This function is a subset of field to be sent in one piece
+  // An alternative to the sendBleData()
+  uint16_t  rowerDataFlags=0b0000101111110;
+  //P1
+  // 0000000000001 - 1   - 0x001 - More Data 0 <!> Present !!! Read the F... Manual !!!
+  // 0000000000010 - 2   - 0x002 - Average Stroke present
+  // 0000000000100 - 4   - 0x004 - Total Distance Present
+  // 0000000001000 - 8   - 0x008 - Instantaneous Pace present
+  // 0000000010000 - 16  - 0x010 - Average Pace Present
+  // 0000000100000 - 32  - 0x020 - Instantaneous Power present
+  // 0000001000000 - 64  - 0x040 - Average Power present
+  // 0000010000000 - 128 - 0x080 - Resistance Level present
+  //P2
+  // 0000100000000 - 256 - 0x080 - Expended Energy present
+  // 0001000000000 - 512 - 0x080 - Heart Rate present
+  // 0010000000000 - 1024- 0x080 - Metabolic Equivalent present
+  // 0100000000000 - 2048- 0x080 - Elapsed Time present
+  // 1000000000000 - 4096- 0x080 - Remaining Time present
+
+  //  0000101111110  
+
+  //  C1  Stroke Rate             uint8     Position    2  + (After the Flag 2bytes)
+  //  C1  Stroke Count            uint16    Position    3  +
+  //  C2  Average Stroke Rate     uint8     Position    5  +
+  //  C3  Total Distance          uint24    Position    6  +
+  //  C4  Instantaneous Pace      uint16    Position    9  +
+  //  C5  Average Pace            uint16    Position    11 +
+  //  C6  Instantaneous Power     sint16    Position    13 +
+  //  C7  Average Power           sint16    Position    15 +
+  //  C8  Resistance Level        sint16    Position    17 -
+  //  C9  Total Energy            uint16    Position    19 +
+  //  C9  Energy Per Hour         uint16    Position    21 +
+  //  C9  Energy Per Minute       uint8     Position    23 +
+  //  C10 Heart Rate              uint8     Position    24 -
+  //  C11 Metabolic Equivalent    uint8     Position    25 -
+  //  C12 Elapsed Time            uint16    Position    26 -
+  //  C13 Remaining Time          uint16    Position    28 -
+
+  unsigned char cRower[22];
+
+  cRower[0]=rowerDataFlags & 0x000000FF;
+  cRower[1]=(rowerDataFlags & 0x0000FF00) >> 8;
+  rdKpi.strokeRate=rdKpi.strokeRate*2;
+  cRower[2] = rdKpi.strokeRate & 0x000000FF;
+  
+  cRower[3] = rdKpi.strokeCount & 0x000000FF;
+  cRower[4] = (rdKpi.strokeCount & 0x0000FF00) >> 8;
+  
+  cRower[5] = rdKpi.averageStokeRate & 0x000000FF;
+  
+  cRower[6] = rdKpi.totalDistance &  0x000000FF;
+  cRower[7] = (rdKpi.totalDistance & 0x0000FF00) >> 8;
+  cRower[8] = (rdKpi.totalDistance & 0x00FF0000) >> 16;
+  
+  cRower[9] = rdKpi.instantaneousPace & 0x000000FF;
+  cRower[10] = (rdKpi.instantaneousPace & 0x0000FF00) >> 8;
+  
+  cRower[11] = rdKpi.averagePace & 0x000000FF;
+  cRower[12] = (rdKpi.averagePace & 0x0000FF00) >> 8;
+
+  cRower[13] = rdKpi.instantaneousPower & 0x000000FF;
+  cRower[14] = (rdKpi.instantaneousPower & 0x0000FF00) >> 8;
+
+  cRower[15] = rdKpi.averagePower & 0x000000FF;
+  cRower[16] = (rdKpi.averagePower & 0x0000FF00) >> 8;
+
+  cRower[17] = rdKpi.totalEnergy & 0x000000FF;
+  cRower[18] = (rdKpi.totalEnergy & 0x0000FF00) >> 8;
+
+  cRower[19] = rdKpi.energyPerHour & 0x000000FF;
+  cRower[20] = (rdKpi.energyPerHour & 0x0000FF00) >> 8;
+
+  cRower[21] = rdKpi.energyPerMinute & 0x000000FF;
+
+  gatt.setChar(fitnessMachineRowerDataId, cRower, 22);
+
+}
+
+
 void sendBleData(){
   // Due the size limitation of the message in the BLE Stack of the NRF
   // the message will be split in 2 parts with the according Bitfield (read the spec :) )
@@ -402,8 +481,6 @@ void sendBleData(){
 
   gatt.setChar(fitnessMachineRowerDataId, cRower, 19);
 
-  //delay(25);
-
   // Send the P2 part of the Message
   cRower[0]=rowerDataFlagsP2 & 0x000000FF;
   cRower[1]=(rowerDataFlagsP2 & 0x0000FF00) >> 8;
@@ -438,22 +515,15 @@ int s4KpiTurn=0;
 int usbCounterCycle=0;
 
 struct s4MemoryMap{
-   char  desc[32];
-   char  addr[4]; // 3+1
-   char  msize[2]; //1+1
+  char  desc[32];
+  char  addr[4]; // 3+1
+  char  msize[2]; //1+1
   int * kpi; // void cause can be in or lon int
   int base; // 10 for Decimal, 16 for Hex used by strtol
 };
 
 #define S4SIZE 8
 struct s4MemoryMap s4mmap[S4SIZE]; 
-
-/*={
-    { "instantaneousPower","088","D",&rdKpi.instantaneousPower,16},
-    { "totalDIstance","057","D",&rdKpi.totalDistance,16},
-    { "strokeCount","140","D",&rdKpi.strokeCount,16},
-    { "strokeRate","1A9","D",&rdKpi.strokeRate,16}
-  };*/
 
 void setup(){
   
@@ -466,53 +536,53 @@ void setup(){
 
   
 
-    sprintf(s4mmap[0].desc,"instantaneousPower");
-    sprintf(s4mmap[0].addr,"088");
-    sprintf(s4mmap[0].msize,"D");
-    s4mmap[0].kpi=&rdKpi.instantaneousPower;
-    s4mmap[0].base=16;
+  sprintf(s4mmap[0].desc,"instantaneousPower");
+  sprintf(s4mmap[0].addr,"088");
+  sprintf(s4mmap[0].msize,"D");
+  s4mmap[0].kpi=&rdKpi.instantaneousPower;
+  s4mmap[0].base=16;
 
-    sprintf(s4mmap[1].desc,"totalDistance");
-    sprintf(s4mmap[1].addr,"057");
-    sprintf(s4mmap[1].msize,"D");
-    s4mmap[1].kpi=&rdKpi.totalDistance;
-    s4mmap[1].base=16;
+  sprintf(s4mmap[1].desc,"totalDistance");
+  sprintf(s4mmap[1].addr,"057");
+  sprintf(s4mmap[1].msize,"D");
+  s4mmap[1].kpi=&rdKpi.totalDistance;
+  s4mmap[1].base=16;
 
-    sprintf(s4mmap[2].desc,"strokeCount");
-    sprintf(s4mmap[2].addr,"140");
-    sprintf(s4mmap[2].msize,"D");
-    s4mmap[2].kpi=&rdKpi.strokeCount;
-    s4mmap[2].base=16;
+  sprintf(s4mmap[2].desc,"strokeCount");
+  sprintf(s4mmap[2].addr,"140");
+  sprintf(s4mmap[2].msize,"D");
+  s4mmap[2].kpi=&rdKpi.strokeCount;
+  s4mmap[2].base=16;
 
-    sprintf(s4mmap[3].desc,"strokeRate");
-    sprintf(s4mmap[3].addr,"1A9");
-    sprintf(s4mmap[3].msize,"S");
-    s4mmap[3].kpi=&rdKpi.strokeRate;
-    s4mmap[3].base=16;
+  sprintf(s4mmap[3].desc,"strokeRate");
+  sprintf(s4mmap[3].addr,"1A9");
+  sprintf(s4mmap[3].msize,"S");
+  s4mmap[3].kpi=&rdKpi.strokeRate;
+  s4mmap[3].base=16;
 
-    sprintf(s4mmap[4].desc,"elapsedTime");
-    sprintf(s4mmap[4].addr,"1E8");
-    sprintf(s4mmap[4].msize,"D");
-    s4mmap[4].kpi=&rdKpi.elapsedTime;
-    s4mmap[4].base=16;
+  sprintf(s4mmap[4].desc,"elapsedTime");
+  sprintf(s4mmap[4].addr,"1E8");
+  sprintf(s4mmap[4].msize,"D");
+  s4mmap[4].kpi=&rdKpi.elapsedTime;
+  s4mmap[4].base=16;
 
-    sprintf(s4mmap[5].desc,"elapsedTimeSec");
-    sprintf(s4mmap[5].addr,"1E1");
-    sprintf(s4mmap[5].msize,"S");
-    s4mmap[5].kpi=&rdKpi.elapsedTimeSec;
-    s4mmap[5].base=10;
+  sprintf(s4mmap[5].desc,"elapsedTimeSec");
+  sprintf(s4mmap[5].addr,"1E1");
+  sprintf(s4mmap[5].msize,"S");
+  s4mmap[5].kpi=&rdKpi.elapsedTimeSec;
+  s4mmap[5].base=10;
 
-    sprintf(s4mmap[6].desc,"elapsedTimeMin");
-    sprintf(s4mmap[6].addr,"1E2");
-    sprintf(s4mmap[6].msize,"S");
-    s4mmap[6].kpi=&rdKpi.elapsedTimeMin;
-    s4mmap[6].base=10;
+  sprintf(s4mmap[6].desc,"elapsedTimeMin");
+  sprintf(s4mmap[6].addr,"1E2");
+  sprintf(s4mmap[6].msize,"S");
+  s4mmap[6].kpi=&rdKpi.elapsedTimeMin;
+  s4mmap[6].base=10;
 
-    sprintf(s4mmap[7].desc,"elapsedTimeHour");
-    sprintf(s4mmap[7].addr,"1E3");
-    sprintf(s4mmap[7].msize,"S");
-    s4mmap[7].kpi=&rdKpi.elapsedTimeHour;
-    s4mmap[7].base=10;
+  sprintf(s4mmap[7].desc,"elapsedTimeHour");
+  sprintf(s4mmap[7].addr,"1E3");
+  sprintf(s4mmap[7].msize,"S");
+  s4mmap[7].kpi=&rdKpi.elapsedTimeHour;
+  s4mmap[7].base=10;
   
   if (UsbH.Init())
     SerialDebug.println("USB host failed to initialize");
@@ -542,30 +612,24 @@ void writeCdcAcm(char str[]){
 
     if (ll > 0) {
       /* sending to USB CDC ACM */
-//#ifdef DEBUG
+#ifdef DEBUG
       SerialDebug.print("<");
       SerialDebug.print(buf);
-//#endif
-//delay(25);
-//rcode = AcmSerial.SndData(ll,(uint8_t*) buf);
-//delay(25);
+#endif
 
-//delay(75);
       for (int i=0;i<ll;i++){
         c=buf[i];
-//#ifdef DEBUG
+#ifdef DEBUG
         SerialDebug.write(c);
-//#endif
-        delay(10);          // By the S4 Spec Wait 25msec between each char sent
+#endif
+        delay(10);          // By the S4 Spec Wait 10/25msec between each char sent
         rcode = AcmSerial.SndData(1, &c);
         if (rcode)
           ErrorMessage<uint8_t>(PSTR("SndData"), rcode);
-        //delay(25);
-     } 
+      } 
     }
-    //readCdcAcm();  
   }else{
-    SerialDebug.print("ACM NRS\n");
+    SerialDebug.print("USB CDC ACM not ready for write\n");
   }
 }
 
@@ -582,35 +646,24 @@ void readCdcAcm(){
     char buf[64];    
     uint16_t rcvd = sizeof(buf);
 
-    //int totalRcvd=0;
-    rcode = AcmSerial.RcvData(&rcvd, (uint8_t *)buf);
-    //while (rcvd>0 && totalRcvd<128){
-    //  totalRcvd+=rcvd;
-     
-      if(rcvd){ //more than zero bytes received
-        buf[rcvd]='\0';
+    rcode = AcmSerial.RcvData(&rcvd, (uint8_t *)buf); 
+    if(rcvd){ //more than zero bytes received
+      buf[rcvd]='\0';
 
 #ifdef DEBUG
-        SerialDebug.print(">");
-        SerialDebug.print(rcvd);
-        SerialDebug.print(",");
-        SerialDebug.print(buf);
+      SerialDebug.print(">");
+      SerialDebug.print(rcvd);
+      SerialDebug.print(",");
+      SerialDebug.print(buf);
 #endif
-        parseS4ReceivedData(buf,rcvd);
-      }
-      rcode = AcmSerial.RcvData(&rcvd, (uint8_t *)buf);
-      if (rcode && rcode != USB_ERRORFLOW)
-        ErrorMessage<uint8_t>(PSTR("Ret"), rcode);
-    
-   //   if (totalRcvd>128){ // This is important cause PXX Message are Priorty and we need IRD Message
-  //#ifdef DEBUG
-  //       SerialDebug.println("Break!");
-  //#endif
-  //      break;
-    //  }
-    //} 
+      parseS4ReceivedData(buf,rcvd);
+    }
+    rcode = AcmSerial.RcvData(&rcvd, (uint8_t *)buf);
+    if (rcode && rcode != USB_ERRORFLOW)
+      ErrorMessage<uint8_t>(PSTR("Ret"), rcode);
+     
   }else{
-    SerialDebug.print("ACM NRR\n");
+    SerialDebug.print("USB CDC ACM not ready for read\n");
   }
 }
 
@@ -619,23 +672,22 @@ void parseS4ReceivedData(char data[],int len){
   SerialDebug.println(data);
   if (len>2){
 
-      if (data[len-1]=='\n' && data[len-2]=='\r'){
-        data[len-2]='\0';
-      }else{
+    if (data[len-1]=='\n' && data[len-2]=='\r'){
+      data[len-2]='\0';
+    }
+    else{
 #ifdef DEBUG
-        SerialDebug.println("");
+      SerialDebug.println("");
 #endif
-      }
-      decodeS4Message(data);
+    }
+    decodeS4Message(data);
   }else{
       SerialDebug.println("Inv msg");
    }
 }
 
 int asciiHexToInt(char hex[],int base){
-  
   //https://stackoverflow.com/questions/23576827/arduino-convert-a-string-hex-ffffff-into-3-int
-  
   int number = (int) strtol( &hex[0], NULL, base);
   return number;
 }
@@ -672,29 +724,29 @@ void decodeS4Message(char cmd[]){
         if (strlen(cmd)>6){
           for (int i=0;i<S4SIZE;i++){
           
-          if (!strncmp(cmd+3,s4mmap[i].addr,3)){
-            *s4mmap[i].kpi=asciiHexToInt(cmd+6,s4mmap[i].base);
-//#ifdef DEBUG
-            SerialDebug.print(s4mmap[i].desc);
-            SerialDebug.print(",");
-            SerialDebug.print(s4mmap[i].addr);
-            SerialDebug.print("=");
-            SerialDebug.println(*s4mmap[i].kpi);
-//ê#endif            
-            break;  
+            if (!strncmp(cmd+3,s4mmap[i].addr,3)){
+              *s4mmap[i].kpi=asciiHexToInt(cmd+6,s4mmap[i].base);
+#ifdef DEBUG
+              SerialDebug.print(s4mmap[i].desc);
+              SerialDebug.print(",");
+              SerialDebug.print(s4mmap[i].addr);
+              SerialDebug.print("=");
+              SerialDebug.println(*s4mmap[i].kpi);
+#endif            
+              break;  
+            }
           }
         }
-       }
       }
-
-      break;
-     case 'O':
       break;
      
-     case 'P':
+    case 'O':
       break;
      
-     case 'S':
+    case 'P':
+      break;
+     
+    case 'S':
       break;
   }
 }
@@ -704,52 +756,45 @@ unsigned long previousTime=0;
 unsigned long pCycleTime=0;
 
 void loop(){
-  
+  // <!> Remember delay is Evil !!! 
+  // readCdcAcm is here at the top for a reason 
 
   currentTime=millis();
-  readCdcAcm();
-  UsbH.Task();
+  readCdcAcm(); 
+  UsbH.Task();      // Todo: test if UsbH has to be at the very top
   
   if (s4InitFlag==false && AcmSerial.isReady() ){
     if (s4SendUsb==false){
       writeCdcAcm((char*)"USB");
       s4SendUsb=true;
     }
-    //readCdcAcm();
   }else{
-    //if ((currentTime-pCycleTime)>REFRESH_DATA_TIME){
-      //pCycleTime=currentTime;
-           // No Matter What we read the port
-    //}
+    
     if ((currentTime-previousTime)>REFRESH_DATA_TIME){
       previousTime=currentTime;
       if ( s4InitFlag==true && bleConnectionStatus==true ){ // Get S4 Data Only if BLE is Connected
 
         char cmd[7];
-        //for (int i=0;i<4;i++){
-          sprintf(cmd,"IR%s%s",s4mmap[s4KpiTurn].msize,s4mmap[s4KpiTurn].addr);
-          writeCdcAcm(cmd);
-          //delay(25);
-          //readCdcAcm();
-        //} 
+        sprintf(cmd,"IR%s%s",s4mmap[s4KpiTurn].msize,s4mmap[s4KpiTurn].addr); // One KPI per cycle or SAMD21 will lost message
+        writeCdcAcm(cmd);
+
         s4KpiTurn++;
         if (s4KpiTurn==4)
         s4KpiTurn=0; 
       }
       
       // Send BLE Data 
-
-      if (ble.isConnected() && s4InitFlag==true  ){ // Start Sending BLE Data only when BLE is connected and when S4 is fully initialized
+      if (ble.isConnected() && s4InitFlag==true ){ // Start Sending BLE Data only when BLE is connected and when S4 is fully initialized
         if (bleConnectionStatus==false)
           SerialDebug.println("BLE:Connected");   
         bleConnectionStatus=true;
         
-    #ifdef USE_FAKE_DATA
+#ifdef USE_FAKE_DATA
         sendFakeBleDataP1();
         sendFakeBleDataP2();
-    #else
+#else
         sendBleData();
-    #endif
+#endif
 
       }else{
         if (bleConnectionStatus==true)
@@ -761,8 +806,8 @@ void loop(){
   
   if (!AcmSerial.isReady()){
     usbCounterCycle++;
-    if (usbCounterCycle>10){         // Need 5 Cycle of USB.task to init the USB
-      SerialDebug.println("USB Serial is not ready sleep for 10sec");
+    if (usbCounterCycle>10){         // Need 32 Cycle of USB.task to init the USB
+      SerialDebug.println("USB Serial is not ready sleep for 1 sec");
       delay(1000);
       usbCounterCycle=0;
     }
